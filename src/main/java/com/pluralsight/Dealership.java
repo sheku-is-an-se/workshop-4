c352d9b5a87a88ca53012a54e2e32c4871a8f158
sheku-is-a-se <sheku2026@gmail.com>
2026-05-08 16:25:40 -0400

    Add classes and methods

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,58 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+
+
+    }
+
+    public List<Vehicle> getVehiclesByPrice (int min, int max) {
+        return null;
+    }
+
+
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByYear(int min, int max) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByColor(String color) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMileage(int min, int max) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByType(String vehicleType) {
+        return null;
+    }
+
+    public List<Vehicle> getAllVehicles() {
+        return null;
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+
+    }
+
+    public List<Vehicle> removeVehicle(Vehicle vehicle) {
+        return null;
+    }
 }
--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,6 @@
 package com.pluralsight;
 
 public class DealershipFileManager {
+
+    String INVENTORY_FILE_PATH = "src/main/java/resources/inventory.csv";
 }
